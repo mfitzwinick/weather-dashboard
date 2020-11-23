4069ce233c4c53e52f4c91a461904bdd43ae5d71
Michele Fitzgerald-Winick <mfitzwinick@gmail.com>
2020-11-23 13:46:43 -0800

    finish ajax calls and readme

--- README.md
+++ README.md
@@ -1,45 +1,47 @@
-# weather-dashboard
+# Weather Dashboard
 
-# 06 Server-Side APIs: Weather Dashboard
+## Description/Purpose
 
-Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+Weather Dashboard is a go-to resource for big city travelors. It is an invaluable tool for anyone needing to understand today's weather along with the five day forecast.  It easily stores the user's travel history for future use.
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that will provide basic setup and usage instructions. Use `localStorage` to store any persistent data.
 
-## User Story
+## Technologies Used
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+Languages used: 
+- HTML
+- CSS
+- JAVASCRIPT / JQUERY
 
-## Acceptance Criteria
+The site uses the following ajax calls from the open weather API:
+- "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=0aee5e8e45d5880c3c161511ef0363e8"
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-WHEN I open the weather dashboard
-THEN I am presented with the last searched city forecast
-```
+- "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=0aee5e8e45d5880c3c161511ef0363e8"
 
-The following image demonstrates the application functionality:
+- "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial&APPID=0aee5e8e45d5880c3c161511ef0363e8"
 
-![weather dashboard demo](./Assets/06-server-side-apis-homework-demo.png)
 
-## Review
+The site also pulls from the following third-party APIs:
+- JQuery
+- Bootstrap with Popper.js
+- Moment.js
 
-You are required to submit the following for review:
+## Features
 
-* The URL of the deployed application.
+Weather Dashboard allows the user to type in a city of their choice so that they can view current weather information.  The input will be saved locally for the user so that they can easily click on cities that have been searched in the past
 
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
+The search results for today's weather will appear in the center of the page, and will include the following information: city name, today's date, current temperature, wind speed, humidity percentage, UV index, and weather description and icon.   A sidebar of will include the user's search history with clickable links.  The lower portion of the page will provide information on the five day forecast.  Each day of the five day forecast will include a descriptive icon, the temperature and humity forecast.
+
+
+## Functionality
+
+For the user, the site is easy to navigate. The user can simply input a city name in order to get an output of current weather information.
+
+## File Structure
+
+The site is built with  the following pages:
+
+-  index.html
+-  style.css
+-  script.js
+
+
--- script.js
+++ script.js
@@ -1,14 +1,215 @@
-$(document).ready(function(){ 
+$(document).ready(function () {
+  const m = moment()
+  console.log(m);
+  dateDisplay = m.format("dddd" + ", " + "MMMM" + " " + "Do" + " " + "YYYY");
+  $("#currentDay").text(dateDisplay)
+  var nextDay = moment().add(1, 'day').format('L')
+  $("#dateDayOne").html(nextDay)
+  var nextDay2 = moment().add(2, 'day').format('L')
+  $("#dateDayTwo").html(nextDay2)
+  var nextDay3 = moment().add(3, 'day').format('L')
+  $("#dateDayThree").html(nextDay3)
+  var nextDay4 = moment().add(4, 'day').format('L')
+  $("#dateDayFour").html(nextDay4)
+  var nextDay5 = moment().add(5, 'day').format('L')
+  $("#dateDayFive").html(nextDay5)
+  
+  //set variable for weather icons//
+  var clear = "http://openweathermap.org/img/wn/01d@2x.png"
+  var clouds = "http://openweathermap.org/img/wn/02d@2x.png"
+  var rain = "http://openweathermap.org/img/wn/10d@2x.png"
+  var snow = "http://openweathermap.org/img/wn/13d@2x.png"
 
+  //pull city history from local storage//
+  function getStorage() {
+    var values = []
+    keys = Object.keys(localStorage),
+      i = keys.length;
+    while (i--) {
+      values.push(localStorage.getItem(keys[i]))
+    }
+    for (let i = 0; i < values.length; i++) {
+      $("#cityList").append(`<button type="submit" class="col-md-12 btn-success preCity" id="cityHistory" data-city="${values[i]}">` + values[i] + "</button>");
+    }
+    return values;
+  }
+  getStorage();
 
+//event listener for search buttons//
+  $(".preCity").on("click", function (event) {
+    event.preventDefault()
+    $(".form-control").val($(this).attr("data-city"))
+    $("#searchBtn").click()
+  })
 
+  $("#searchBtn").on("click", function (event) {
+    event.preventDefault();
+    var citySearch = $(".form-control").val();
+  
+//ajax call for today's weather//
+    var todayWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=0aee5e8e45d5880c3c161511ef0363e8"
+    $.ajax({
+      url: todayWeather,
+      method: "GET"
+    }).then(function (response) {
+      var icon = clear
+      switch (response.weather[0].main) {
+        case "Clear":
+          icon = clear
+          break;
+        case "Clouds":
+          icon = clouds
+          break;
+        case "Rain":
+          icon = rain
+          break;
+        case "Snow":
+          icon = snow
+          break;
+      }
+      var tempF = ((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+      $(".cityName").html("<h2>" + response.name + "</h2>")
+      $(".tempF").html("<h3>" + "temp:  " + tempF + "    F°" + "</h3>")
+      $(".wind").html("<p>" + "wind speed:  " + response.wind.speed + "  MPH" + "</p>")
+      $(".humidity").html("<p>" + "humidity:  " + response.main.humidity + "%" + "</p>")
+      $(".description").html("<p>" + response.weather[0].description + "</p>")
+      $("#icon").empty()
+      $("#icon").append(`<img src="${icon}"></img>`)
+    });
+//ajax call for Five Day Forecast//
+    var weatherFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&units=imperial&APPID=0aee5e8e45d5880c3c161511ef0363e8"
+    $.ajax({
+      url: weatherFiveDay,
+      method: "GET"
+    }).then(function (response) {
+      var humidFive=[];
+      var tempFive=[];
+      var iconFive=[];
+      //input icons and data into each day for Five Day Forecast//
+      for (let i=0; i < response.list.length; i+=8) {
+        humidFive.push(response.list[i].main.humidity);
+        tempFive.push(response.list[i].main.temp);
+        iconFive.push(response.list[i].weather[0].main)
+        }
+  $("#infoDayOne").html("<p>" + "humidity:  " + humidFive[0]+ "%" + "</p>");
+  $("#infoDayTwo").html("<p>" + "humidity:  " + humidFive[1] + "%" + "</p>");
+  $("#infoDayThree").html("<p>" + "humidity:  " + humidFive[2] + "%" + "</p>");
+  $("#infoDayFour").html("<p>" + "humidity:  " + humidFive[3] + "%" + "</p>");
+  $("#infoDayFive").html("<p>" + "humidity:  " + humidFive[4] + "%" + "</p>");
+  $("#infoDayOne").prepend("<p>" + "temp:  " + tempFive[0]+ "  F°" + "</p>");
+  $("#infoDayTwo").prepend("<p>" + "temp:  " + tempFive[1] + "  F°" + "</p>");
+  $("#infoDayThree").prepend("<p>" + "temp:  " + tempFive[2] + "  F°" + "</p>");
+  $("#infoDayFour").prepend("<p>" + "temp:  " + tempFive[3] + "  F°" + "</p>");
+  $("#infoDayFive").prepend("<p>" + "temp:  " + tempFive[4] + "  F°" + "</p>");
+         
+       var iconD1= clear;
+        switch (iconFive[0]) {
+          case "Clear":
+            iconD1 = clear
+            break;
+          case "Clouds":
+            iconD1 = clouds
+            break;
+          case "Rain":
+            iconD1 = rain
+            break;
+          case "Snow":
+            iconD1 = snow
+            break;
+        }
+            $("#infoDayOne").prepend(`<img src="${iconD1}"></img>`);
 
+            var iconD2= clear;
+            switch (iconFive[1]) {
+              case "Clear":
+                iconD2 = clear
+                break;
+              case "Clouds":
+                iconD2 = clouds
+                break;
+              case "Rain":
+                iconD2 = rain
+                break;
+              case "Snow":
+                iconD2 = snow
+                break;
+            }
+            $("#infoDayTwo").prepend(`<img src="${iconD2}"></img>`);
 
+            var iconD3= clear;
+            switch (iconFive[2]) {
+              case "Clear":
+                iconD3 = clear
+                break;
+              case "Clouds":
+                iconD3 = clouds
+                break;
+              case "Rain":
+                iconD3 = rain
+                break;
+              case "Snow":
+                iconD3 = snow
+                break;
+            }
+            $("#infoDayThree").prepend(`<img src="${iconD3}"></img>`);
 
+            var iconD4= clear;
+            switch (iconFive[3]) {
+              case "Clear":
+                iconD4 = clear
+                break;
+              case "Clouds":
+                iconD4 = clouds
+                break;
+              case "Rain":
+                iconD4 = rain
+                break;
+              case "Snow":
+                iconD4 = snow
+                break;
+            }
+            $("#infoDayFour").prepend(`<img src="${iconD4}"></img>`);
 
+            var iconD5= clear;
+            switch (iconFive[4]) {
+              case "Clear":
+                iconD5 = clear
+                break;
+              case "Clouds":
+                iconD5 = clouds
+                break;
+              case "Rain":
+                iconD5 = rain
+                break;
+              case "Snow":
+                iconD5 = snow
+                break;
+            }
+            $("#infoDayFive").prepend(`<img src="${iconD5}"></img>`);
 
+   //ajax call for UV index// 
+      var lat = response.city.coord.lat;
+      var lon = response.city.coord.lon;
+      var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=0aee5e8e45d5880c3c161511ef0363e8"
+      $.ajax({
+        url: uvUrl,
+        method: "GET"
+      }).then(function (response) {
+        //  console.log(response);
+        $(".uv").html(`<p id="uvBackground">` + `UV Index:  ` + response.value + "</p>");
+        if (response.value <= 2) {
+          $("#uvBackground").addClass("low")
+        } else if (response.value <= 5) {
+          $("#uvBackground").addClass("med")
+        } else if (response.value <= 7) {
+          $("#uvBackground").addClass("high")
+        } else {
+          $("#uvBackground").addClass("veryhigh")
+        };
+      });
+//put a new city search into local storage//
+      localStorage.setItem(citySearch, citySearch);
 
-
-
-
+    })
+  })
 });
